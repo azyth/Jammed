dc7a8c237f66238f92661b1031cede0a835b1c76
azyth <jh836@cornell.edu>
2015-03-10 17:13:36 -0400

    basic class structure

---
+++ Jammed/src/Jammed.java
@@ -0,0 +1,15 @@
+
+public class Jammed {
+
+	
+	public static void main(String[] argv){
+		System.out.println("Welcome to Jammed");
+		System.out.println("..._____...");
+		System.out.println("..|~~~~~|..");
+		System.out.println("..|-JAM-|..");
+		System.out.println("..|-----|..");
+		System.out.println("...........");
+		System.out.println("Your passwords have been preserved...");
+		
+	}
+}
