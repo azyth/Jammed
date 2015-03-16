860297b7814baf5cfc10b34c21c28be355b525de
Dan Etter <dettervt@gmail.com>
2015-03-16 14:16:32 -0400

    Double checking my git plays nicely with Eclipse
    It doesn’t.

--- Jammed/src/Jelly.java
+++ Jammed/src/Jelly.java
@@ -2,4 +2,8 @@
 public class Jelly {
 	// Dan
 	// Server-side stuffs
+	public static void main(String[] args){
+		int port = 5430;
+		System.out.println("Jelly Server Created on port "+port);
+	}
 }
