800e4ca44561f065d1e2eabbec8cf88dbf7bd449
marcospedreiro <mvp34@cornell.edu>
2015-03-17 00:13:07 -0400

    Need to finish writing files for v1 to be done

---
+++ Jammed/src/Constants.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Marcos on 3/16/15.
+ */
+
+/**
+ * Constants that are needed by multiple classes
+ * */
+public class Constants {
+    private Constants() {}
+
+    /* Used in the DB interface */
+    public static final double USER_DATA = 99;
+    public static final double USER_PWD_FILE = 98;
+    public static final double USER_LOG = 97;
+    /* End DB constants */
+}
--- Jammed/src/DB.java
+++ Jammed/src/DB.java
@@ -3,46 +3,166 @@
    Class to perform file management for the server
 
    NOTES:
-    1) As of 3/16/15, interface is not final
+    1) As of 3/16/15, interface is not final.
+    2) Constants are used to read specific types of files.
  */
-
-import java.io.File;
+import static java.nio.file.StandardOpenOption.*;
+import java.nio.file.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DB {
-	// Fields here
-    //
 
-    /* Methods */
+    /* Testing */
+    public static void main(String[] args) {
+        boolean res = initialize();
+        boolean mkUsr = newUser("test001");
+        boolean srchUsr = searchUser("test001");
+        System.out.println(srchUsr);
 
-    // May replace with a constructor
-    public boolean initialize() {
-        // To do
-        return false;
+        String dataToLog = "Hello world";
+        boolean didWriteLog = writeLog(dataToLog);
+        System.out.println(didWriteLog);
+
+        byte[] fileData = readFile("test001", Constants.USER_DATA);
+        System.out.println(fileData);
     }
 
-    public File readFile(String uid, String fileType) {
+    /** Method Description here */
+    public static boolean initialize() {
+        Path server = Paths.get("root/server/");
+        Path user = Paths.get("root/users/");
+
+        if(Files.exists(server) && Files.exists(user)) {
+            System.out.println("DB has already been initialized");
+            return true;
+        }
+        // Create root and server folder
+        try {
+            Files.createDirectories(server);
+        } catch(Exception e) {
+            System.out.println("Could not create root and server dir!");
+            return false;
+        }
+        // Create user folder
+        try {
+            Files.createDirectories(user);
+        } catch(Exception e) {
+            System.out.println("Could not create root and user dir!");
+            return false;
+        }
+
+        System.out.println("Successfully made dirs");
+        return true;
+    }
+
+    /** Method Description here */
+    public static boolean newUser(String uid) {
+        Path newUser = Paths.get("root/users/" + uid + "/");
+
+        if(Files.exists(newUser)) {
+            System.out.println("User already exists");
+            return false;
+        }
+        // Create root and server folder
+        try {
+            Files.createDirectories(newUser);
+        } catch(Exception e) {
+            System.out.println("Could not create new user: " + uid);
+            return false;
+        }
+
+        return true;
+    }
+
+    /** Method Description here */
+    public static boolean searchUser(String uid) {
+        Path user = Paths.get("root/users/" + uid + "/");
+        return Files.exists(user);
+    }
+
+    /** Method Description here */
+    public static boolean writeLog(String dataToLog) {
+        Path lgname = Paths.get("root/server/log.txt");
+        if(!Files.exists(lgname)) {
+            try {
+                Files.createFile(lgname);
+            } catch(Exception e) {
+                System.out.println("Could not create log.");
+                return false;
+            }
+        }
+
+        // Convert the string to a byte array.
+        byte data[] = dataToLog.getBytes();
+        try {
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(lgname, CREATE, APPEND));
+            out.write(data, 0, data.length);
+        } catch (IOException x) {
+            System.out.println("Could not write data to log.");
+            return false;
+        }
+        return true;
+
+    }
+
+    /** Method Description here */
+    public static byte[] readFile(String uid, double fileType) {
         // To do
+        String fname;
+        if(fileType == Constants.USER_DATA) {
+            fname = "USERDATA.bin";
+        } else if(fileType == Constants.USER_PWD_FILE) {
+            fname = "PWD.bin";
+        } else if(fileType == Constants.USER_LOG) {
+            fname = "LOG.bin";
+        } else {
+            // Not a valid file, do nothing
+            return null;
+        }
+
+        Path fileToRead = Paths.get("root/users/" + uid + "/" + uid + fname);
+        if(Files.exists(fileToRead)) {
+            byte[] fileArray;
+            try {
+                fileArray = Files.readAllBytes(fileToRead);
+            } catch (Exception e) {
+                System.out.println("Could not read User Data");
+                return null;
+            }
+            return fileArray;
+        }
+
         return null;
     }
 
-    public boolean writeFile(String uid, String fileType, byte[] fileData) {
+    /** Method Description here */
+    public static boolean writeFile(String uid, double fileType, File fileData) {
         // To do
+        String fname;
+        if(fileType == Constants.USER_DATA) {
+            fname = "USERDATA.bin";
+        } else if(fileType == Constants.USER_PWD_FILE) {
+            fname = "PWD.bin";
+        } else if(fileType == Constants.USER_LOG) {
+            fname = "LOG.bin";
+        } else {
+            // Not a valid file, do nothing
+            return false;
+        }
+
+        Path fileToRead = Paths.get("root/users/" + uid + "/" + uid + fname);
+        if(Files.exists(fileToRead)) {
+            return false;
+        }
         return false;
     }
 
-    public boolean newUser(String uid) {
-        // To do
-        return false;
-    }
 
-    public boolean writeLog(byte[] dataToLog) {
-        // To do
-        return false;
-    }
 
-    public boolean searchUser(String uid) {
-        // To do
-        return false;
-    }
 
-}
+
+
+} // END CLASS
